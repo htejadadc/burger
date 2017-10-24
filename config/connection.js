d022455cf896c297364e45c804120f1fc90800e8
Hernan Tejada <hernan.tejada@gmail.com>
2017-10-24 17:17:17 -0400

    Some CSS Improvements

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,10 @@ var connection;
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	connection = mysql.createConnection({
-		port: 3306,
+	connection = mysql.createConnection({	
 		host: "localhost",
 		user: "root",
-		password: "",
+		password: "raga73=MS_17",
 		database: "burgers_db"
 	});
 };
