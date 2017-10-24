e40792818b628ae9d3b53104469d3a5acdb682be
Hernan Tejada <hernan.tejada@gmail.com>
2017-10-24 12:28:39 -0400

    connection repair

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({		
-		host: "localhost",
-		user: "root",
-		password: "",
+		host: "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+		user: "vxvpmzw9j9toxez2",
+		password: "cd5bh9ihub1pjca1",
 		database: "burgers_db"
 	});
 };
